969f8c9e236e560b955a4f477c9b9f31d444cdc1
stephaniiema <54160019+stephaniiema@users.noreply.github.com>
2024-05-15 15:26:58 -0500

    fixed commas inc sv

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import { YMCAscraper } from "./WebScrapers.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+// sheetJS import
+import { writeFile, utils, read, writeFileXLSX } from "xlsx";
 
 const App = () => {
   const [urlValue, setUrlValue] = useState("");
@@ -41,17 +43,9 @@ const App = () => {
   function download() {
     if (resultFile) {
       console.log("file exists")
-      const file = resultFile;
-      const link = document.createElement("a");
-      const url = URL.createObjectURL(file);
-
-      link.href = url;
-      link.download = file.name;
-      document.body.appendChild(link);
-      link.click();
-
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(url);
+      var wb = utils.book_new();
+      utils.book_append_sheet(wb, resultFile, "Sheet1");
+      writeFile(wb, "SheetJSExportAOA.csv");
     } else {
       console.log("error try again")
       console.log(file)
@@ -68,25 +62,22 @@ const App = () => {
       if (document.getElementById("download-button").style.display == "inline") {
         document.getElementById("download-button").style.display = "none";
       }
-
-      // if (document.getElementById("downloadButton")) {
-      //   document.getElementById("downloadButton").remove();
-      // }
       var sdata = await YMCAscraper(urlValue)
       console.log(sdata)
       setScrapingData(sdata)
 
       var fileData = Array();
-      const header = "Folder_Name,Program_Name,Program_Description,Logo_URL,Category,Program_Capacity,Min_Age,Max_Age,Meeting_Type,Location_Name,Address,City,State,Zipcode,Program_URL,Registration_URL,Start_Date,End_Date,Start_Time,End_Time,Registration_Deadline,Contact_Name,Contact_Email,Contact_Phone,Price,Extra_Data,online_address,dosage,internal_id,neighborhood,community,ward";
-      // const header = ["Folder_Name","Program_Name","Program_Description","Logo_URL","Category","Program_Capacity","Min_Age","Max_Age","Meeting_Type","Location_Name","Address","City","State","Zipcode","Program_URL","Registration_URL","Start_Date","End_Date","Start_Time","End_Time","Registration_Deadline","Contact_Name","Contact_Email","Contact_Phone","Price","Extra_Data","online_address","dosage","internal_id","neighborhood","community","ward"];
+      // const header = "Folder_Name,Program_Name,Program_Description,Logo_URL,Category,Program_Capacity,Min_Age,Max_Age,Meeting_Type,Location_Name,Address,City,State,Zipcode,Program_URL,Registration_URL,Start_Date,End_Date,Start_Time,End_Time,Registration_Deadline,Contact_Name,Contact_Email,Contact_Phone,Price,Extra_Data,online_address,dosage,internal_id,neighborhood,community,ward";
+      const header = ["Folder_Name","Program_Name","Program_Description","Logo_URL","Category","Program_Capacity","Min_Age","Max_Age","Meeting_Type","Location_Name","Address","City","State","Zipcode","Program_URL","Registration_URL","Start_Date","End_Date","Start_Time","End_Time","Registration_Deadline","Contact_Name","Contact_Email","Contact_Phone","Price","Extra_Data","online_address","dosage","internal_id","neighborhood","community","ward"];
 
 
 
       sdata.forEach((item) => {
         var row = Array(32).fill("");
-        row[1] = "\"" + item["Title"].replaceAll(",", "\,") + "\"";
-        row[2] = "\"" + item["Description"].replaceAll(",", "\,") + "\"";
-        console.log(row[2])
+        // row[1] = "\"" + item["Title"].replaceAll(",", "\,").replaceAll(/"/g, '\"') + "\"";
+        row[1] = item["Title"];
+        // row[2] = "\"" + item["Description"].replaceAll(",", "\,").replaceAll(/"/g, '\"') + "\"";
+        row[2] = item["Description"];
         row[5] = item["Spots Remaining"].split("of")[1].trim();
         row[6] = item["Min_Age"];
         row[7] = item["Max_Age"];
@@ -96,30 +87,17 @@ const App = () => {
         row[18] = item["Start_Time"];
         row[19] = item["End_Time"];
         row[24] = "Member cost " + item["Member_Cost"] + " Non-member cost " + item["Non_Member_Cost"];
-        fileData.push(row.toString() + "\n");
-        // fileData.push(row);
+        // fileData.push(row.toString() + "\n");
+        fileData.push(row);
         
       })
 
-      // fileData.unshift(header);
-      // fileData = fileData.join("\n")
-      // console.log(fileData)
-      fileData = header + "\n" + fileData.join("");
+      fileData.unshift(header);
+      var ws = utils.aoa_to_sheet(fileData, {bookType: 'xlsx', type: 'string'});
+      setResultFile(ws);
 
-      file = new File([fileData], "dynscraper_data.csv", {
-        type: "text/csv",
-      });
-
-      setResultFile(file);
-
-      if (file) {
-        // const button = document.createElement("Button");
-        // button.innerHTML = "Download CSV";
-        // button.onclick = download;
-        // button.id = "downloadButton";
-        // // find the div with class of 'app'
-        // const app = document.querySelector(".App");
-        // app.appendChild(button);
+      if (ws) {
+        // unhide download button
         document.getElementById("download-button").style.display = "inline";
         console.log("file created")
         
